dfc174ab227008bfc7f11d8f78d1b72d8bd56a11
jalaljamalXD <noob123noob123jjxd@gmail.com>
2022-09-23 04:38:22 -0700

    query

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.db.models import Q, F
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from django.db.models import Value, F, Func
+
 from store.models import Collection, Customer, Order, OrderItem, Product
 
 
 def say_hello(request):
-    queryset = Product.objects.select_related('collection').all()
+    queryset = Customer.objects.annotate(
+        full_name=Func(F('first_name'), F('last_name'))
+    )
 
-    return render(request, 'hello.html', {'name': 'Jalal', 'products': list(queryset)})
+
+
+
+    # queryset = Order.objects.aggregate(count=Count('id'))
+    # queryset = OrderItem.objects.filter(product__id=1).aggregate(units_sold=Sum('quantity'))
+    # queryset = Order.objects.filter(customer_id=1).aggregate(count=Count('id'))
+    # queryset = Product.objects.filter(collection__id=3).aggregate(min=Min('unit_price'), avg=Avg('unit_price'),max=Max('unit_price'))
+    return render(request, 'hello.html', {'name': 'Jalal', 'products': queryset})
