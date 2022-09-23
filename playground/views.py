46c1aa4f3b97fad1b2ad08d249de720575d49391
jalaljamalXD <noob123noob123jjxd@gmail.com>
2022-09-23 02:20:49 -0700

    query

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F
 from store.models import Collection, Customer, Order, OrderItem, Product
 
 
-
-
-
 def say_hello(request):
-    queryset = Order.objects.filter(customer__id=1)
-
+    queryset = Product.objects.select_related('collection').all()
 
     return render(request, 'hello.html', {'name': 'Jalal', 'products': list(queryset)})
