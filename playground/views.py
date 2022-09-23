0a52b52768e205747bd5ba3f512319d2858ddbf5
jalaljamalXD <noob123noob123jjxd@gmail.com>
2022-09-23 05:04:45 -0700

    query

--- playground/views.py
+++ playground/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from django.db.models import Q, F
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from django.db.models import Value, F, Func
+from django.db.models.functions import Concat
 
 from store.models import Collection, Customer, Order, OrderItem, Product
 
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(
-        full_name=Func(F('first_name'), F('last_name'))
-    )
 
 
+    
+    # queryset = Customer.objects.annotate(
+    #     full_name=Func(F('first_name'), Value(
+    #         ' '), F('last_name'), function='CONCAT')
+    # )
 
+    # queryset = Customer.objects.annotate(
+    #     full_name=Concat('first_name', Value(' '), 'last_name')
+    # )
 
     # queryset = Order.objects.aggregate(count=Count('id'))
     # queryset = OrderItem.objects.filter(product__id=1).aggregate(units_sold=Sum('quantity'))
